4e9a9a730f951cbe4d543bef413662d8ab309b5b
bananots <natasyarianaa@gmail.com>
2025-07-19 14:03:48 +0700

    halo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,17 @@
-import bg from './bg.jpg'
+import bg from './bf.jpg'
 
-import picture from './picture.png'
+import picture1 from './p1.jpg'
+import picture2 from './p2.jpg'
+import picture3 from './p3.jpg'
+import picture4 from './p4.jpg'
+import picture5 from './p5.jpg'
+import picture6 from './p6.jpg'
+import picture7 from './p7.jpg'
+import picture8 from './p8.jpg'
+import picture9 from './p9.jpg'
+import picture10 from './p10.jpg'
+
+
 
 import confetti from './confetti.gif'
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 import Home from "./Home";
 import Picture from "./Picture";
-import Card from "./Card";
-import SectionWrapper from "./SectionWrapper";
 import Cake from "./Cake";
 import Present from "./Present";
 
@@ -9,8 +7,6 @@ import Present from "./Present";
 export {
     Home,
     Picture,
-    Card,
-    SectionWrapper,
     Cake,
     Present
 }
